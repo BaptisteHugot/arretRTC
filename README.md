b411b5e1203bea2f647c116d65aec39402844783
Baptiste Hugot <baptistehugot@gmail.com>
2019-05-30 16:47:06 +0200

    Initial work

---
+++ README.md
@@ -0,0 +1,25 @@
+# arretRTC
+Ce programme affiche une carte montrant les différentes zones où la technologie RTC sera arrêtée en France, par date d'arrêt et par zone géographique. Cette carte est basée sur les données publiques disponibles sur les sites d'[Orange](https://reseaux.orange.fr/projets/modernisation-telephonie-fixe) et de la [FFT](https://www.fftelecoms.org/nos-travaux-et-champs-dactions/reseaux/arret-fin-rtc-premieres-zones-devoilees-plaques-10-2018/).
+
+## Exemple
+Un exemple complet est disponible sur ma page personnelle GitHub (https://baptistehugot.github.io/arretRTC/).
+
+## Ecrit avec
+* [Javascript] - Le langage de programmation utilisé pour gérer la gestion de la carte et des bibliothèques
+* [HTML] - Le langage de programmation utilisé pour afficher la page Internet
+* [CSS] - Le langage de programmation utilisé pour gérer les styles de la page Internet
+
+## Bibliothèques utilisées
+* [Leaflet](https://leafletjs.com/) - La bibliothèque utilisée pour afficher la carte
+* [Leaflet-Ajax](https://github.com/calvinmetcalf/leaflet-ajax) - La bibliothèque utilisée pour gérer l'affichage des zones contenues dans le fichier GeoJson
+* [Leaflet-Geosearch](https://github.com/smeijer/leaflet-geosearch) - La bibliothèque utilisée pour permettre la recherche par commune
+* [Leaflet-Locatecontrol](https://github.com/domoritz/leaflet-locatecontrol) - La bibliothèque utilisée pour permettre la géolocalisation de l'utilisateur
+
+## Versions
+[SemVer](http://semver.org/) est utilisé pour la gestion de versions. Pour connaître les versions disponibles, veuillez vous référer aux [étiquettes disponibles dans ce dépôt](https://github.com/BaptisteHugot/arretRTC/releases/).
+
+## Auteurs
+* **Baptiste Hugot** - *Travail initial* - [BaptisteHugot](https://github.com/BaptisteHugot)
+
+## Licence
+Ce projet est disponible sous licence MIT. Veuillez lire le fichier [LICENSE.md](LICENSE.md) pour plus de détails.
---
+++ carte_arret_rtc.js
@@ -0,0 +1,204 @@
+/**
+ * Fichier générant la carte qui sera affichée montrant les plaques concernées par l'arrêt du RTC, par département et par date d'arrêt
+ */
+
+// Initialisation des variables
+var latitude = 46.76306;
+var longitude = 2.424722;
+var map = null;
+var GeoSearchControl = window.GeoSearch.GeoSearchControl;
+var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+var geojsonLayer;
+var info;
+
+/**
+ * Fonction définissant la couleur qui est affectée à une année particulière
+ * @param L'année concernée
+ */
+function getColor(year) {
+  return year == 2021 ? '#0B3D91' :
+    year == 2023 ? '#FF4500' :
+    '#D8B2D8';
+}
+
+/**
+ * Fonction qui définit le style qui sera affiché en fonction d'une donnée particulière
+ * @param Le fichier GeoJson en entrée
+ */
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.data_arret_rtc_DATE),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  };
+}
+
+/*
+ * Foncion qui définit un listener pour lorsque la souris est sur une des couches
+ * @param Évènement
+ */
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+/*
+ * Fonction qui définit ce qui se passe lorsque la souris quitte une des couches
+ * @param Évènement
+ */
+function resetHighlight(e) {
+  geojsonLayer.resetStyle(e.target);
+  info.update();
+}
+
+/*
+ * Fonction qui va permettre de zoomer sur l'élément lorsqu'un clic est détecté sur ce dernier
+ * @param Évènement
+ */
+function zoomToFeature(e) {
+    macarte.fitBounds(e.target.getBounds());
+}
+
+/**
+ * Fonction qui ajoute les listeners pour chaque couche de la carte
+ * @param Fonctionnalités
+ * @param Couches
+ */
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    //click: zoomToFeature
+  });
+}
+
+/**
+ * Fonction d'initialisation de la carte
+ */
+function initMap() {
+  // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
+  map = L.map('map').setView([latitude, longitude], 6);
+  // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+  var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    // Il est toujours bien de laisser le lien vers la source des données
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    minZoom: 1,
+    maxZoom: 20
+  });
+
+  // On lit les données contenues dans le fichier geojson
+  geojsonLayer = new L.GeoJSON.AJAX("./carte_arret_rtc.json", {
+    style: style,
+    onEachFeature: onEachFeature
+  });
+
+  // On définit la légende de la carte
+  var legend = L.control({
+    position: 'bottomright'
+  });
+
+  // On gère la géolocalisation de l'utilisateur
+  var location = L.control.locate({
+    position: 'topleft',
+    setView: 'untilPanOrZoom',
+    flyTo: false,
+    cacheLocation: true,
+    drawMarker: true,
+    drawCircle: false,
+    showPopup: false,
+    keepCurrentZoomLevel: false
+  });
+
+  // On définit le fournisseur sur lequel on va s'appuyer pour effectuer les recherches d'adresse
+  var provider = new OpenStreetMapProvider({
+    params: {
+      countrycodes: 'fr'
+    }, // On restreint uniquement les recherches pour la France
+  });
+
+  // On définit le module de recherche
+  var searchControl = new GeoSearchControl({
+    provider: provider,
+    showMarker: true,
+    showPopup: false,
+    marker: {
+      icon: new L.Icon.Default,
+      draggable: false,
+      interactive: false
+    },
+    maxMarkers: 1,
+    retainZoomLevel: false,
+    animateZoom: true,
+    autoClose: true,
+    searchLabel: "Entrez l'adresse",
+    keepResult: true
+  });
+
+  /**
+   * On ajoute la légende à la carte
+   * @param la carte où la légende sera ajoutée
+   */
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [],
+      labels = [2021, 2023];
+
+    // On boucle sur toutes les valeurs et on génère une étiquette avec la bonne couleur pour chaque valeur
+    for (var i = 0; i < labels.length; i++) {
+      div.innerHTML += '<i style="background:' + getColor(labels[i]) + '"></i> ' + labels[i] + '<br>';
+    }
+    return div;
+  };
+
+  info = L.control();
+
+  /**
+   * On ajoute les informatios à la carte
+   * @param la carte où les informations seront ajoutées
+   */
+  info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // On créé une section avec la classe info
+    this.update();
+    return this._div;
+  }
+
+  /**
+   * Fonction qui met à jour les informations en fonction des propriétés passées
+   * @param Propriétés
+   */
+  info.update = function (props) {
+    this._div.innerHTML = '<h4>Date arrêt RTC</h4>' + (props ?
+      '<b>' + props.data_arret_rtc_DEP + '</b><br />' + props.data_arret_rtc_DATE : '');
+  };
+
+
+  // On ajoute toutes les couches à la carte
+  osmLayer.addTo(map);
+  geojsonLayer.addTo(map);
+  legend.addTo(map);
+  map.addControl(searchControl);
+  location.addTo(map);
+  info.addTo(map);
+}
+
+/**
+ * Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+ */
+window.onload = function () {
+  initMap();
+};
